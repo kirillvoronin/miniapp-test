5655381665b7f09a0b42d950abad42624c432a83
kirillvoronin <voronin@yahoo.com>
2025-08-09 15:40:10 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +8,5 @@ export default defineConfig({
     strictPort: true,
     host: true,
   },
+  base: "/miniapp-test/",
 });
-
-
